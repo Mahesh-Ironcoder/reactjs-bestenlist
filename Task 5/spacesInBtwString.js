8fe220f3d583d5926b7e73b2f450cc618365e938
Mahesh Kancharla <maheshkancharla614@gmail.com>
2020-12-01 20:21:26 +0530

    just another commit

--- Task 5/spacesInBtwString.js
+++ Task 5/spacesInBtwString.js
@@ -1,6 +1,6 @@
 String.prototype.bespacify = function(){
     var letters = this.valueOf().split("")
-    return letters.reduce((prev, curr) => { return prev + curr + " " },"")
+    return letters.reduce((prev, curr) => prev + curr + " ", "")
 }
 
 var demo = new String("Mahesh")
